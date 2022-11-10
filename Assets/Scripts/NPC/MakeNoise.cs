9678a555fa1eb61a70c9f0ee27dc7cfae7a13d05
marlonkerstens <marlon_kerstens@live.nl>
2022-11-10 15:06:06 +0100

    Start with FSM, Patrol en Alert

--- Assets/Scripts/NPC/MakeNoise.cs
+++ Assets/Scripts/NPC/MakeNoise.cs
@@ -13,28 +13,18 @@ public class MakeNoise : MonoBehaviour
         //when space bar pressed, make noise
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            spawnSound();         
             makeNoise();
         }
     }
-
-    void spawnSound(){
-        GameObject sound = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-        sound.transform.position = transform.position;
-        sound.transform.Translate(0, -0.5f, 0);
-        sound.transform.localScale = new Vector3(noiseLevel, 0.1f, noiseLevel);
-        sound.GetComponent<Renderer>().material.color = Color.red;
-}
-
+    
     void makeNoise(){
         Collider[] foundObjects = Physics.OverlapSphere(transform.position, noiseLevel/2);
         foreach(var currentObject in foundObjects)
         {
-            if (currentObject.tag == "Enemy")
+            if (currentObject.CompareTag("Enemy"))
             {
                 float distance = Vector3.Distance(transform.position, currentObject.transform.position);
                 SoundSource source = new SoundSource(gameObject, noiseLevel / distance);
-                Debug.Log("Noise: " + noiseLevel / distance + "For Object: " + currentObject.name);
                 currentObject.transform.gameObject.SendMessage("NoiseReceived", source,
                     SendMessageOptions.DontRequireReceiver);
             }
