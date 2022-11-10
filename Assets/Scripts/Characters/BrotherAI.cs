e27d3eee789f27d586855be215fbad2bb136dfbf
Jelco van der Straaten <jelcovanderstraaten@gmail.com>
2022-11-10 11:52:41 +0100

    toevoegen van mogelijkheid veranderen snelheid

--- Assets/Scripts/Characters/BrotherAI.cs
+++ Assets/Scripts/Characters/BrotherAI.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class BrotherAI : MonoBehaviour
 {
     [SerializeField] Transform _walkLocation;
+
+    [SerializeField] float _runSpeed = 20;
+    [SerializeField] float _walkSpeed = 10;
+
     private NavMeshAgent _navMeshAgent;
     void Awake()
     {
@@ -14,13 +17,15 @@ public class BrotherAI : MonoBehaviour
 
     void Update()
     {
-        WalkToLocation(_walkLocation);
+        MoveToLocation(_walkLocation, _runSpeed);
     }
 
-    private void WalkToLocation(Transform walkLocation){
+    private void MoveToLocation(Transform walkLocation, float speed){
+        _navMeshAgent.speed = speed;
         _navMeshAgent.SetDestination(walkLocation.position);
     }
 
+//action will become an enum
     public void PingBrother(object action){
         // if(action instanceof HideAction ){
         // HideAction hideAction = action;
