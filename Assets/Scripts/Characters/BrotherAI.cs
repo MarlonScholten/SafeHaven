dd11c25dab1789039d0d5bd90353480695127c46
Jelco van der Straaten <jelcovanderstraaten@gmail.com>
2022-11-10 15:32:52 +0100

    updated brotherAI script to pickup and drop objects

--- Assets/Scripts/Characters/BrotherAI.cs
+++ Assets/Scripts/Characters/BrotherAI.cs
@@ -5,30 +5,75 @@ using UnityEngine.AI;
 public class BrotherAI : MonoBehaviour
 {
     [SerializeField] Transform _walkLocation;
+    [SerializeField] GameObject _pickup;
 
-    [SerializeField] float _runSpeed = 20;
-    [SerializeField] float _walkSpeed = 10;
+    [SerializeField] float _pickupDistance;
 
     private NavMeshAgent _navMeshAgent;
+
+    [SerializeField] Transform _hand;
+
+    private GameObject inventory = null;
+
     void Awake()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        MoveToLocation(_walkLocation, _runSpeed);
+        PickupAction(_pickup.transform, _pickup);           
+        
+        if(inventory != null){
+            HoldItem();
+            MoveToLocation(_walkLocation);
+        }
+
+        if(_navMeshAgent.remainingDistance < 1 && inventory != null){
+            DropItem();
+        }
     }
 
-    private void MoveToLocation(Transform walkLocation, float speed){
-        _navMeshAgent.speed = speed;
+    private void MoveToLocation(Transform walkLocation){
         _navMeshAgent.SetDestination(walkLocation.position);
     }
 
+//action will become an enum
     public void PingBrother(object action){
-
+        // if(action instanceof HideAction ){
+        // HideAction hideAction = action;
+        // hideAction.hide();
+        // hideNearLocation(location);
+        // }
+        // if(action instanceof PanicHide){
+        // panicHide();
+        // }
     }
 
+    public void PickupAction(Transform objectLocation, GameObject objectToPickup){
+        MoveToLocation(objectLocation);
+        if(Vector3.Distance(_pickup.transform.position, transform.position) < _pickupDistance){
+            PickupItem(_pickup);
+        }     
+    }
 
-    
+    public void PickupItem(GameObject item){
+        if(inventory is not null){
+            DropItem();
+        }
+        inventory = item;
+    }
+
+    public void DropItem(){
+      inventory.transform.parent = null;
+      inventory.GetComponent<Rigidbody>().isKinematic = false;
+      inventory = null;  
+    }
+
+    public void HoldItem(){
+        inventory.transform.position = _hand.position;
+        inventory.transform.rotation = _hand.rotation;
+        inventory.transform.parent = _hand.transform;
+        inventory.GetComponent<Rigidbody>().isKinematic = true;
+    }
 }
