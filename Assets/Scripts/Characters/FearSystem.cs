00eeed9c8ac4427844e59876088e435c78893c9d
Jelco van der Straaten <jelcovanderstraaten@gmail.com>
2022-11-15 15:34:59 +0100

    definetely works already ;)

--- Assets/Scripts/Characters/FearSystem.cs
+++ Assets/Scripts/Characters/FearSystem.cs
@@ -7,15 +7,15 @@ public class FearSystem : MonoBehaviour
     protected float _fear = 0f;
     private float _maxFearLevel = 1;
     private float _comfort = 0.1f;
-    private float _fearIncement = 0.01f;
+    private float _fearIncement = 0.02f;
 
     private float _maxFearDistance = 20f;
 
-    private float _viewRadius = 50f;
+    private float _viewRadius = 30f;
     [SerializeField] LayerMask _EnemyMask;
 
     void Update(){
-
+        checkEnemyEncounter();
     }
 
     public void checkEnemyEncounter(){
@@ -23,16 +23,20 @@ public class FearSystem : MonoBehaviour
         foreach(Collider enemy in enemies){
             Transform enemyPos = enemy.transform;
             Vector3 dirToEnemy = (enemyPos.position - transform.position).normalized;
-            float distance = Vector3.Distance(transform.position,enemyPos.position);
-                if(!Physics.Raycast(transform.position, dirToEnemy, distance, _EnemyMask)){
-                    UpdateFearLevel(distance);
-                }
+            float distance = Vector3.Distance(transform.position, enemyPos.position);
+            Debug.Log("Distance: " + distance);
+            if(Physics.Raycast(transform.position, dirToEnemy, out RaycastHit hit, distance)){
+                if(hit.collider.tag == "Enemy"){
+                        UpdateFearLevel(distance);
+                }                    
+            }
         }
     }
 
     public void UpdateFearLevel(float distance){
         if(distance < _maxFearDistance && _fear < _maxFearLevel){
-            _fear += (_fearIncement - distance);
+            _fear += (_fearIncement - (distance / 1000f)) * Time.deltaTime;
+            Debug.Log(_fear);
         }
     }
 
