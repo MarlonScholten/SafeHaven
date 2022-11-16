0379712e51eb8042209868fae5aa6c4fcf61db52
Kajirinn <kajirinnn@gmail.com>
2022-11-16 17:11:26 +0100

    Completed most of the waypoint and interaction behaviour.
    Still need to test the Extension class, and add some bits and bobs.

--- Assets/Scripts/Enviroment/InteractableBehaviour.cs
+++ Assets/Scripts/Enviroment/InteractableBehaviour.cs
@@ -1,26 +1,117 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
+/// <summary>
+/// Custom interactable event, passes <see cref="WaypointBehaviour"/> as an additional parameter.
+/// </summary>
+[Serializable]
+public class InteractableEventHandler : UnityEvent<WaypointBehaviour>
+{
+}
+
+/// <summary>
+/// Handles how the main interactable component behaves, and is the top most level class in the interactable hierarchy.
+/// </summary>
 public class InteractableBehaviour : MonoBehaviour
 {
     // Public.
+    [Header("Events")]
+    public InteractableEventHandler OnTraversal;
 
     // Private.
+    [Header("References")]
+    [SerializeField]
+    private TMP_Text _instructions;
 
+    private PlayerInput _input;
+    private GameObject _player;
+    [SerializeField]
+    private List<WaypointBehaviour> _waypointsInRange;
+
+    /// <summary>
+    /// Standard awake, sets references & events.
+    /// </summary>
     private void Awake()
     {
-        foreach (var test in GetComponentsInChildren<WaypointBehaviour>())
+        _input = new();
+        _waypointsInRange = new();
+
+        _input.Player.Interact.performed += OnInteractInput;
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+        HandleWaypointEvents();
     }
 
     private void Start()
     {
-        
+        // TODO : Create a custom text iput for the active control input.
     }
 
-    private void Update()
+    /// <summary>
+    /// Sets the behaviour for the waypoint <see cref="WaypointBehaviour.OnWaypointEnter"/> and <see cref="WaypointBehaviour.OnWaypointExit"/>.
+    /// Removes and adds the active waypoints to the <see cref="_waypointsInRange"/>, whilst handling the visibility of <see cref="_instructions"/>.
+    /// </summary>
+    private void HandleWaypointEvents()
     {
-        
+        // Loop through all waypoints.
+        foreach (WaypointBehaviour waypoint in GetComponentsInChildren<WaypointBehaviour>())
+        {
+            // Remove the waypoint when applicable.
+            waypoint.OnWaypointExit += () =>
+            {
+                if (_waypointsInRange.Contains(waypoint))
+                    _waypointsInRange.Remove(waypoint);
+
+                _instructions.gameObject.SetActive(false);
+            };
+            // Add the waypoint when applicable.
+            waypoint.OnWaypointEnter += () =>
+            {
+                if (!_waypointsInRange.Contains(waypoint))
+                    _waypointsInRange.Add(waypoint);
+
+                _instructions.gameObject.SetActive(true);
+            };
+        }
+    }
+
+    /// <summary>
+    /// Enables the <see cref="PlayerInput"/> whenever <see cref="InteractableBehaviour"/> is active.
+    /// </summary>
+    public void OnEnable()
+    {
+        _input.Enable();
+    }
+
+    /// <summary>
+    /// Enables the <see cref="PlayerInput"/> whenever <see cref="InteractableBehaviour"/> is inactive.
+    /// </summary>
+    public void OnDisable()
+    {
+        _input.Disable();
+    }
+
+    /// <summary>
+    /// Input function, gets called whenever the player presses the corresponding input key.
+    /// Fetches the closest waypoint, and invokes the corresponding traversal event to be handled.
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (_waypointsInRange.Count == 0)
+            return;
+
+        Vector3 originPos = _player.transform.position;
+        GameObject closest = _waypointsInRange.Select(x => x.gameObject).GetClosestGameObject(originPos);
+
+        // TODO : Discuss where the responsibility for the OneWay lies.
+
+        OnTraversal?.Invoke(closest.GetComponent<WaypointBehaviour>());
     }
 }
--- Assets/Scripts/Enviroment/WaypointBehaviour.cs
+++ Assets/Scripts/Enviroment/WaypointBehaviour.cs
@@ -2,24 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Handles how the waypoints behave, and is the bottom most class in the interactable hierarchy.
+/// </summary>
 public class WaypointBehaviour : MonoBehaviour
 {
-    [Header("Waypoint link")]
-    public GameObject Waypoint1;
-    public GameObject Waypoint2;
-    public bool OneWay;
+    // Public.
+    public GameObject Waypoint1 { get => gameObject; }
+    public GameObject Waypoint2 { get => _linkedWaypoint; }
+    public bool OneWay { get => _oneWay;  }
 
-    public delegate void WaypointEventHandler(WaypointBehaviour waypoint);
+    // Delegate.
+    public delegate void WaypointEventHandler();
     public event WaypointEventHandler OnWaypointEnter;
     public event WaypointEventHandler OnWaypointExit;
 
+    // Private.
+    [Header("References")]
+    [SerializeField]
+    private GameObject _linkedWaypoint;
+    [SerializeField]
+    private bool _oneWay;
+
+    /// <summary>
+    /// Invokes the <see cref="OnWaypointEnter"/> event once triggered.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
-        OnWaypointEnter?.Invoke(this);
+        OnWaypointEnter?.Invoke();
     }
 
+    /// <summary>
+    /// Invokes the <see cref="OnWaypointExit"/> event once triggered.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
-        OnWaypointExit?.Invoke(this);
+        OnWaypointExit?.Invoke();
     }
 }
