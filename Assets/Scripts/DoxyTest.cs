30bf2578628bbcd2b6ac0d0586b2cbb83bc90fe1
Thomas <TCJ.vandenOever@student.han.nl>
2022-11-18 14:56:23 +0100

    Revamped the template

--- Assets/Scripts/DoxyTest.cs
+++ Assets/Scripts/DoxyTest.cs
@@ -5,13 +5,63 @@ using UnityEngine;
 /// <summary>
 /// A very cool description of what to do with this script and what it needs, what other components, tags, layers and/or scripts are needed for this script to work.
 /// </summary>
-/// <list type="Components, tags, layers and/or scripts">
-/// <param name="componentname">This component need to be on the object or objects.</param>
-/// <param name="tagname">This tag needs to be on the object or objects.</param>
-/// <param name="scriptname">This script needs to be connected to the object or objects.</param>
+/// <list type="table">
+///	    <listheader>
+///         <term>On what GameObject</term>
+///         <term>Type</term>
+///         <term>Name of type</term>
+///         <term>Description</term>
+///     </listheader>
+///     <item>
+///         <term>The GameObject this thing need to be on for this script to work</term>
+///		    <term>What type of this is. Component, Script, Tag or Layer?</term>
+///         <term>The name of thing</term>
+///		    <term>A discription of why this thing is need</term>
+///	    </item>
+///	    <item>
+///         <term>The same GameObject as this script is on</term>
+///		    <term>Component</term>
+///         <term>CharacterController</term>
+///		    <term>Script uses the move() function of the character controller to move the player</term>
+///	    </item>
+///     <item>
+///		    <term>The debugging GameObject</term>
+///	     	<term>Script</term>
+///	       	<term>DebugPosition.cs</term>
+///	       	<term>Usefull for the debugger to know the position of the player</term>
+///	    </item>
+///	    <item>
+///		    <term>Child GameObject of this GameObject</term>
+///		    <term>Tag</term>
+///		    <term>Friendly</term>
+///		    <term>Used because this script looks for these tags to see if it applies to them</term>
+///	    </item>
+///	    <item>
+///		    <term>Parent GameObject of this GameObject</term>
+///		    <term>Layer</term>
+///		    <term>non_interacteble</term>
+///		    <term>Used so the pinging system doens't ping the player itself</term>
+///		</item>
 /// </list>
+/// 
+
+
+
+
 public class DoxyTest : MonoBehaviour
 {
+    /// <summary>
+    /// Set to true to see if you are happy
+    /// </summary>
+    [SerializeField]
+    public bool happy = false;
+
+    /// <summary>
+    /// Used for the sensitivity of the mouse, ranges form 0 to 100
+    /// </summary>
+    [Range(0.0f, 100.0f)]
+    public float sensitivity = 20.0f;
+    
     /// <summary>
     /// Adds two numbers togheter
     /// </summary>
