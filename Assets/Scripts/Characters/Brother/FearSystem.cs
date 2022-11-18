93dc4b85c4b33d6ddb7514783cae35b4f94d7f66
Jelco van der Straaten <jelcovanderstraaten@gmail.com>
2022-11-18 14:25:25 +0100

    brother has an fov again

--- Assets/Scripts/Characters/Brother/FearSystem.cs
+++ Assets/Scripts/Characters/Brother/FearSystem.cs
@@ -12,6 +12,7 @@ public class FearSystem : MonoBehaviour
     [SerializeField]private float _maxFearDistance = 20f;
 
     [SerializeField]private float _viewRadius = 30f;
+    [SerializeField]private float _viewAngle = 140f;
     [SerializeField] LayerMask _EnemyMask;
 
     void Update(){
@@ -28,13 +29,40 @@ public class FearSystem : MonoBehaviour
             Transform enemyPos = enemy.transform;
             Vector3 dirToEnemy = (enemyPos.position - transform.position).normalized;
             float distance = Vector3.Distance(transform.position, enemyPos.position);
-            if(Physics.Raycast(transform.position, dirToEnemy, out RaycastHit hit, distance)){
-                if(hit.collider.tag == "Enemy"){
-                        UpdateFearLevel(distance);
+            if(Vector3.Angle(transform.forward, dirToEnemy) < _viewAngle / 2){
+                if(Physics.Raycast(transform.position, dirToEnemy, out RaycastHit hit, distance)){
+                    if(hit.collider.tag == "Enemy"){
+                            UpdateFearLevel(distance);
+                    }
                 }                    
             }
         }
     }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGLobal){
+        if(!angleIsGLobal){
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+            // foreach(Collider spot in hidingSpots){
+            // Transform hidingSpot = spot.transform;
+            // Vector3 dirToObject = (hidingSpot.position - transform.position).normalized;
+            //     //Check if the spot is in the brothers view
+            // if(Vector3.Angle(transform.forward, dirToObject) < _viewAngle / 2){
+            //     float distanceToSpot = Vector3.Distance(transform.position,hidingSpot.position);
+
+            //     if(!Physics.Raycast(transform.position, dirToObject, distanceToSpot, _obstacleMask)){
+            //             //If there is not a current closest spot, then set one
+            //         if(closestHidingSpot == null){
+            //             closestHidingSpot = hidingSpot;
+            //         }
+            //             //Check if other location is closer than current closest location
+            //         else if(distanceToSpot < Vector3.Distance(closestHidingSpot.transform.position, transform.position)){
+            //                 closestHidingSpot = hidingSpot;
+            //         }
+            //     }
 
     public void UpdateFearLevel(float distance){
         if(distance < _maxFearDistance && _fear < _maxFearLevel){
--- Assets/Scripts/Characters/Brother/FindHidingSpot.cs
+++ Assets/Scripts/Characters/Brother/FindHidingSpot.cs
@@ -26,7 +26,6 @@ public class FindHidingSpot : MonoBehaviour
                 }
             }
         }
-        Debug.Log("Spot: " + bestSpot.transform);
         return bestSpot.transform;    
     }
 
