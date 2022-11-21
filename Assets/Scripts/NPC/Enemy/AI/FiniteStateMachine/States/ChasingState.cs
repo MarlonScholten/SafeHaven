5fd2c3e73f0b8ded6c50a7b4d7f532aff6338de7
HugUlf <hugo.ulfman@gmail.com>
2022-11-21 12:10:56 +0100

    comments enzo you know

--- Assets/Scripts/NPC/Enemy/AI/FiniteStateMachine/States/ChasingState.cs
+++ Assets/Scripts/NPC/Enemy/AI/FiniteStateMachine/States/ChasingState.cs
@@ -27,12 +27,15 @@ public class ChasingState : MonoBehaviour
     /// </summary>
     public void Update_Chasing()
     {
+        //If the player is not in vision and has not been for 3 seconds, the enemy goes back to investigate.
         if(!_stateManager.CheckVision() && (_stateManager.timePlayerLastSpotted + _stateManager.enemyAiScriptableObject.chaseTimeWhenNotSeen) < Time.time)
         {
+            _stateManager.spottedPlayerLastPosition = _stateManager.spottedPlayer.transform.position;
             CustomEvent.Trigger(gameObject, "Investigate");
         }
         //get distance between player and enemy
         float distance = Vector3.Distance(_stateManager.spottedPlayer.transform.position, transform.position);
+        //If the distance between the player and enemy is less than the set distance, the enemy catches the player.
         if (distance < _stateManager.enemyAiScriptableObject.catchDistance)
         {
             _stateManager.catchChild();
@@ -44,12 +47,8 @@ public class ChasingState : MonoBehaviour
     /// </summary>
     public void FixedUpdate_Chasing()
     {
-        if (_stateManager.CheckVision())
-        {
-            _stateManager.navMeshAgent.SetDestination(_stateManager.spottedPlayer.transform.position);
-        }
-
-        if (!_stateManager.CheckVision() && (_stateManager.timePlayerLastSpotted + _stateManager.enemyAiScriptableObject.chaseTimeWhenNotSeen) > Time.time)
+        //If the player is in vision the enemy moves towards the player. If the player is not in vision but has been in vision in the last 3 seconds, the enemy moves towards the player.
+        if (_stateManager.CheckVision() || (!_stateManager.CheckVision() && (_stateManager.timePlayerLastSpotted + _stateManager.enemyAiScriptableObject.chaseTimeWhenNotSeen) > Time.time))
         {
             _stateManager.navMeshAgent.SetDestination(_stateManager.spottedPlayer.transform.position);
         }
@@ -61,6 +60,7 @@ public class ChasingState : MonoBehaviour
     public void Exit_Chasing()
     {
         _stateManager.CheckPlayerPositionReachable(_stateManager.spottedPlayer.transform.position);
-        _stateManager.spottedPlayer = null;
+        //Reset spotted player.
+        //_stateManager.spottedPlayer = null;
     }
 }
