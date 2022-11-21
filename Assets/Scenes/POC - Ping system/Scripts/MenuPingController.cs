a0f9667f0d8f28254e184ef780a0188bfc58e11c
Iris Giezen <IJG.Giezen@student.han.nl>
2022-11-21 15:32:56 +0100

    Refactored and commented double code

--- Assets/Scenes/POC - Ping system/Scripts/MenuPingController.cs
+++ Assets/Scenes/POC - Ping system/Scripts/MenuPingController.cs
@@ -7,37 +7,31 @@ public class MenuPingController : MonoBehaviour
 {
     //TODO Integrate
     //private BrotherAI _brotherAI;
-    
+
+    [SerializeField] private Camera _camera;
+    [SerializeField] private GameObject _markerPrefab;
     [SerializeField] private GameObject _radialMenu;
     [SerializeField] private GameObject _highlightedOption;
-    [SerializeField] private GameObject _markerPrefab;
-    [SerializeField] private Camera _camera;
-
-    private bool _radialMenuIsSetActive = false;
-    private bool extrabool = false;
-
-    private PingType _chosenAction;
-
     [SerializeField] private int _slowmotionFactor = 4;
 
     private PingSystem _pingSystem;
+    private Vector3 _pingPosition;
     private Vector2 _inputMouse;
     private int _selectedOption;
 
     public Text[] options;
     public Text cancel;
     public Color radialMenuNormal, radialMenuOptionHovered, radialMenuCancel;
+    private bool _radialMenuIsSetActive = false;
 
-    private Vector3 _pingPosition;
     private PingType _pingAction;
+    private PingType _chosenAction;
     private const string NotCancelled = "Not cancelled";
     private string _cancelled;
 
-    private const int Zero = 0;
-    private const int One = 1;
-    private const int Two = 2;
     private const float Correction = 10000f;
     private const int StandardTimeFactor = 1;
+    private const int Two = 2;
 
     private const float StartingPointCorrection = 90f;
     private const float DegreesHalf = 180f;
@@ -48,7 +42,8 @@ public class MenuPingController : MonoBehaviour
 
     private void Awake()
     {
-        _pingSystem = new();
+        _pingSystem = new PingSystem();
+
         _pingSystem.Player.MenuPing.performed += OnMenuPing;
         _pingSystem.Player.QuickPing.performed += OnLeftMouseButton;
     }
@@ -57,32 +52,19 @@ public class MenuPingController : MonoBehaviour
     {
         _radialMenu.SetActive(false);
         _cancelled = NotCancelled;
-
     }
 
     private void Update()
     {
-        if (_radialMenuIsSetActive)
-        {
-            ActivateRadialMenu();
-        }
-    }
-
-    private void OnEnable()
-    {
-        _pingSystem.Enable();
-    }
-
-    private void OnDisable()
-    {
-        _pingSystem.Disable();
+        if (!_radialMenuIsSetActive) return;
+        ActivateRadialMenu();
     }
 
     private void ActivateRadialMenu()
     {
         _radialMenu.SetActive(true);
 
-        if (!_radialMenu.activeInHierarchy) return;
+        if (!_radialMenu.activeSelf) return;
         _inputMouse.x = Mouse.current.position.ReadValue().x - Screen.width / Two;
         _inputMouse.y = Mouse.current.position.ReadValue().y - Screen.height / Two;
 
@@ -101,25 +83,8 @@ public class MenuPingController : MonoBehaviour
         }
         else
         {
-            cancel.color = Color.white;
-            for (var i = Zero; i < options.Length; i++)
-            {
-                _degreesPerSegment = DegreesFull / options.Length;
-                if (angle > i * _degreesPerSegment && angle < (i + One) * _degreesPerSegment)
-                {
-                    ControlSegmentHoveredOverOutside(i);
-                }
-                else
-                {
-                    options[i].color = radialMenuNormal;
-                }
-            }
+            ControlSegmentHoveredOutside(angle);
         }
-    }
-
-    private bool CheckInMiddleSegment()
-    {
-        return _inputMouse.x is < SizeCircle and > -SizeCircle && _inputMouse.y is < SizeCircle and > -SizeCircle;
     }
 
     private void ControlSegmentHoveredOverMiddle()
@@ -129,24 +94,32 @@ public class MenuPingController : MonoBehaviour
         options[_selectedOption].color = Color.white;
     }
 
-    private void ControlSegmentHoveredOverOutside(int i)
+    private void ControlSegmentHoveredOutside(float angle)
+    {
+        cancel.color = Color.white;
+        for (var i = 0; i < options.Length; i++)
+        {
+            _degreesPerSegment = DegreesFull / options.Length;
+            if (angle > i * _degreesPerSegment && angle < (i + 1) * _degreesPerSegment)
+            {
+                ControlActionHovered(i);
+            }
+            else
+            {
+                options[i].color = radialMenuNormal;
+            }
+        }
+    }
+
+    private void ControlActionHovered(int i)
     {
         options[i].color = radialMenuOptionHovered;
         _selectedOption = i;
         _highlightedOption.SetActive(true);
         _highlightedOption.transform.rotation = Quaternion.Euler(0, 0, i * -_degreesPerSegment);
 
-        if (Enum.TryParse(options[i].text, out PingType pingType))
-        {
-            _chosenAction = pingType;
-        }
-    }
-
-    private static float SetDegreesFull(float angle)
-    {
-        if (!(angle < Zero)) return angle;
-        angle += DegreesFull;
-        return angle;
+        if (!Enum.TryParse(options[i].text, out PingType pingType)) return;
+        _chosenAction = pingType;
     }
 
     private void OnLeftMouseButton(InputAction.CallbackContext callbackContext)
@@ -162,22 +135,19 @@ public class MenuPingController : MonoBehaviour
         {
             SelectAction();
         }
+
         CloseRadialMenu();
     }
 
-    private void CloseRadialMenu()
+    private void OnMenuPing(InputAction.CallbackContext callbackContext)
     {
-        _radialMenuIsSetActive = false;
-        _radialMenu.SetActive(_radialMenuIsSetActive);
+        if (_radialMenuIsSetActive) return;
+        _radialMenuIsSetActive = true;
+        
+        var ray = GetRayFromCameraToMousePosition();
+        SetPingPosition(ray);
 
-        Time.timeScale = StandardTimeFactor;
-    }
-
-    private void DetermineValueCancelled()
-    {
-        _cancelled = CheckInMiddleSegment()
-            ? "Cancelled"
-            : NotCancelled;
+        Time.timeScale /= _slowmotionFactor;
     }
 
     private void SelectAction()
@@ -188,26 +158,70 @@ public class MenuPingController : MonoBehaviour
         //_brotherAI.PingBrother(_pingAction, _pingPosition)
     }
 
-    private void OnMenuPing(InputAction.CallbackContext callbackContext)
+    private void CloseRadialMenu()
     {
-        if (_radialMenuIsSetActive) return;
-        
-        _radialMenuIsSetActive = true;
-        var ray = _camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        _radialMenuIsSetActive = false;
+        _radialMenu.SetActive(_radialMenuIsSetActive);
+
+        Time.timeScale = StandardTimeFactor;
+    }
+
+    private static float SetDegreesFull(float angle)
+    {
+        if (!(angle < 0)) return angle;
+        angle += DegreesFull;
+        return angle;
+    }
+
+    private bool CheckInMiddleSegment()
+    {
+        return _inputMouse.x is < SizeCircle and > -SizeCircle && _inputMouse.y is < SizeCircle and > -SizeCircle;
+    }
+
+    private void DetermineValueCancelled()
+    {
+        _cancelled = CheckInMiddleSegment()
+            ? "Cancelled"
+            : NotCancelled;
+    }
+    
+    // Double
+    private Ray GetRayFromCameraToMousePosition()
+    {
+        var mousePosition = new Vector2(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y);
+        var ray = _camera.ScreenPointToRay(mousePosition);
+        return ray;
+    }
+    
+    // Double
+    private void SetPingPosition(Ray ray)
+    {
         Debug.DrawRay(ray.origin, ray.direction * Correction, Color.red, 3);
 
         if (!Physics.Raycast(ray.origin, ray.direction * Correction, out var hit)) return;
         _pingPosition = hit.point;
         ShowMarker(_pingPosition);
-
-        Time.timeScale /= _slowmotionFactor;
     }
 
+    // Double
     private void ShowMarker(Vector3 position)
     {
         Instantiate(_markerPrefab, position, Quaternion.identity);
     }
 
+    // Double
+    private void OnEnable()
+    {
+        _pingSystem.Enable();
+    }
+
+    // Double
+    private void OnDisable()
+    {
+        _pingSystem.Disable();
+    }
+
+    // Double
     public Vector3 GetPingLocation()
     {
         return _pingPosition;
--- Assets/Scenes/POC - Ping system/Scripts/QuickPingController.cs
+++ Assets/Scenes/POC - Ping system/Scripts/QuickPingController.cs
@@ -36,28 +36,7 @@ public class QuickPingController : MonoBehaviour
         //TODO Integrate
         //_brotherAI.PingBrother(PingType.Run, _pingPosition)
     }
-
-    private Ray GetRayFromCameraToMousePosition()
-    {
-        var mousePosition = new Vector2(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y);
-        var ray = _camera.ScreenPointToRay(mousePosition);
-        return ray;
-    }
-
-    private void SetPingPosition(Ray ray)
-    {
-        Debug.DrawRay(ray.origin, ray.direction * Correction, Color.red, 3);
-
-        if (!Physics.Raycast(ray.origin, ray.direction * Correction, out var hit)) return;
-        _pingPosition = hit.point;
-        ShowMarker(_pingPosition);
-    }
-
-    private void ShowMarker(Vector3 position)
-    {
-        Instantiate(_markerPrefab, position, Quaternion.identity);
-    }
-
+    
     private void CancelQuickPing(InputAction.CallbackContext callbackContext)
     {
         _quickCancelled = false;
@@ -68,16 +47,43 @@ public class QuickPingController : MonoBehaviour
         _quickCancelled = true;
     }
 
+    // Double
+    private Ray GetRayFromCameraToMousePosition()
+    {
+        var mousePosition = new Vector2(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y);
+        var ray = _camera.ScreenPointToRay(mousePosition);
+        return ray;
+    }
+
+    // Double
+    private void SetPingPosition(Ray ray)
+    {
+        Debug.DrawRay(ray.origin, ray.direction * Correction, Color.red, 3);
+
+        if (!Physics.Raycast(ray.origin, ray.direction * Correction, out var hit)) return;
+        _pingPosition = hit.point;
+        ShowMarker(_pingPosition);
+    }
+
+    // Double
+    private void ShowMarker(Vector3 position)
+    {
+        Instantiate(_markerPrefab, position, Quaternion.identity);
+    }
+
+    // Double
     private void OnEnable()
     {
         _pingSystem.Enable();
     }
 
+    // Double
     private void OnDisable()
     {
         _pingSystem.Disable();
     }
 
+    // Double
     public Vector3 GetPingLocation()
     {
         return _pingPosition;
