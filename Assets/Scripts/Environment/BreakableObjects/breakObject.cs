64b89239dcaf6a13c7dd733988f87ed6a12ec455
marlonkerstens <marlon_kerstens@live.nl>
2022-11-23 13:53:14 +0100

    Finished breakable object

--- Assets/Scripts/Environment/BreakableObjects/breakObject.cs
+++ Assets/Scripts/Environment/BreakableObjects/breakObject.cs
@@ -1,39 +1,47 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace Environment.BreakableObjects
 {
+    /// <summary>
+    /// This script can be used to destroy an object when it is above a certain force threshold.
+    /// It also has an event that can be used to trigger other actions like an sound effect and animation.
+    /// </summary>
+    /// <list type="table">
+    ///	    <listheader>
+    ///         <term>On what GameObject</term>
+    ///         <term>Type</term>
+    ///         <term>Name of type</term>
+    ///         <term>Description</term>
+    ///     </listheader>
+    ///	    <item>
+    ///         <term>Any (eg. Bottle, Window)</term>
+    ///		    <term>Script</term>
+    ///         <term>Script that uses the objectIsBroken UnityEvent to play an animation or sound effect.</term>
+    ///		    <term>The UnityEvent can be used by adding the function of another into the objectIsBroken field in the inspector. To destroy the game object after the animation or sound effect the function DestroyObject can be used.</term>
+    ///	    </item>
+    /// </list>
     public class breakObject : MonoBehaviour
     {
-        [SerializeField] private float thresholdToBreak = 0.5f;
-    
-        [NonSerialized] public UnityEvent objectIsBroken;
+        [SerializeField] private float thresholdToBreak = 20f; // How much force is needed to break the object
+        [SerializeField] private UnityEvent objectIsBroken; // Event to listen to so that other scripts can be triggered when the object is broken
 
-        public breakObject(UnityEvent objectIsBroken)
-        {
-            this.objectIsBroken = objectIsBroken;
+        /// <summary>
+        /// This method is called when the object collides with another object.
+        /// It checks if the force of the collision is greater than the threshold to break.
+        /// It calls the objectIsBroken event if the object is broken so the animation and sounds can be played.
+        /// </summary>
+        /// <param name="col">The object that it collides with.</param>
+        private void OnCollisionEnter(Collision col) {
+            if(col.relativeVelocity.magnitude > thresholdToBreak) objectIsBroken?.Invoke();
         }
-
-        // Start is called before the first frame update
-        void Start()
+        
+        /// <summary>
+        /// This function can be called by an external UnityEvent to destroy the game object.
+        /// </summary>
+        public void DestroyObject()
         {
-       
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            // if(Animation/Sound is finished)
-            // {
-            //     Destroy(gameObject);
-            // }
-        }
-    
-        void OnCollisionEnter(Collision col) {
-            if(col.relativeVelocity.magnitude > thresholdToBreak) {
-                objectIsBroken.Invoke();
-            }
+            Destroy(gameObject);
         }
     }
 }
