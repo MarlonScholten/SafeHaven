4832a2467be52ece274e0c7acbfcbf54d5b51fa2
Tom Cornelissen <tagwcorn@avans.nl>
2022-11-24 14:52:39 +0100

    Update settings menu

--- Assets/Scripts/UI/Menus/Settings menu/SettingsMenu.cs
+++ Assets/Scripts/UI/Menus/Settings menu/SettingsMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,28 @@ using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
     private GameObject _previousMenu;
-    
+
+    [SerializeField]
+    [Tooltip("A reference to the button UI element that exits this menu")]
+    private Button _exitButton;
+
+    private void Start()
+    {
+        _exitButton.onClick.AddListener(CloseMenu);
+        
+        _exitButton.Select();
+    }
+
+    /// <summary>
+    /// Sets the menu that spawned this menu, closing this menu reactivates this previous menu
+    /// </summary>
+    /// <param name="previousMenu">The menu that is reactivated when this menu is closed</param>
     public void SetPreviousMenu(GameObject previousMenu)
     {
         _previousMenu = previousMenu;
     }
 
-    public void CloseMenu()
+    private void CloseMenu()
     {
         _previousMenu.SetActive(true);
         
