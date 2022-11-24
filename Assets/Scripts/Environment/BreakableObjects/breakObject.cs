f8e9b7c157a010475cf136a24832eec0b81e5065
marlonkerstens <marlon_kerstens@live.nl>
2022-11-24 09:33:07 +0100

    Added new comments according to the new wiki 3.0

--- Assets/Scripts/Environment/BreakableObjects/breakObject.cs
+++ Assets/Scripts/Environment/BreakableObjects/breakObject.cs
@@ -4,9 +4,10 @@ using UnityEngine.Events;
 namespace Environment.BreakableObjects
 {
     /// <summary>
-    /// <para> Author: Marlon Kerstens </para>
-    /// <para> Modified by: none </para>
+    /// <br> Author: Marlon Kerstens </br>
+    /// <br> Modified by: none </br>
     /// Description: This script can be used to destroy an object when it is above a certain force threshold. It also has an event that can be used to trigger other actions like an sound effect and animation.
+    /// How To Use: Attach this script to the object you want to break. Set the force threshold and the event that should be triggered when the object is destroyed.
     /// </summary>
     /// 
     /// <list type="table">
@@ -23,7 +24,7 @@ namespace Environment.BreakableObjects
     ///		    <term>The UnityEvent can be used by adding the function of another into the objectIsBroken field in the inspector. To destroy the game object after the animation or sound effect the function DestroyObject can be used.</term>
     ///	    </item>
     /// </list>
-    public class breakObject : MonoBehaviour
+    public class BreakObject : MonoBehaviour
     {
         [Tooltip("How much force is needed to break the object")] [SerializeField] private float thresholdToBreak = 20f; 
         [Tooltip("Event to listen to so that other scripts can be triggered when the object is broken")] [SerializeField] private UnityEvent objectIsBroken;
