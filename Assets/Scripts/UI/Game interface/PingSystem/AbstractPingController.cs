e56f995ae1d28d094c3247a364d64325306c03f1
Thijso26 <76108060+Thijso26@users.noreply.github.com>
2022-11-25 11:45:07 +0100

    added documentation to quickping and abstractping

--- Assets/Scripts/UI/Game interface/PingSystem/AbstractPingController.cs
+++ Assets/Scripts/UI/Game interface/PingSystem/AbstractPingController.cs
@@ -1,25 +1,80 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+/// <summary>
+/// Author: Iris Giezen, Thijs Orsel </para>
+/// Modified by: n/a </para>
+/// Abstract class to prevent duplicate functionality in both of the ping controllers (QuickPingController and MenuPingController).
+/// </summary>
+/// <list type="table">
+///	    <listheader>
+///         <term>On what GameObject</term>
+///         <term>Type</term>
+///         <term>Name of type</term>
+///         <term>Description</term>
+///     </listheader>
+///     <item>
+///         <term>None</term>
+///		    <term>Script</term>
+///         <term>AbstractPingController</term>
+///		    <term>Abstract class to prevent duplicate functionality in the actual ping controllers.</term>
+///	    </item>
+/// </list>
 
 public abstract class AbstractPingController : MonoBehaviour
 {
+    /// <summary>
+    /// Contains a reference to the brotherAI script of the brother in the current scene.
+    /// </summary>
     protected BrotherAI _brotherAI;
     
+    /// <summary>
+    /// Contains a reference to the brother in the current scene, can be set manually.
+    /// </summary>
     [SerializeField] protected GameObject _brother;
+    
+    /// <summary>
+    /// Contains a reference to the camera in the current scene, can be set manually.
+    /// </summary>
     [SerializeField] protected Camera _camera;
+    
+    /// <summary>
+    /// Contains a reference to the prefab of the marker that is shown when a ping is performed.
+    /// </summary>
     [SerializeField] protected GameObject _markerPrefab;
+    
+    /// <summary>
+    /// Contains a reference to the radial menu in the current scene, can be set manually.
+    /// </summary>
     [SerializeField] protected GameObject _radialMenu;
+    
+    /// <summary>
+    /// Contains a value that represents the duration in seconds a marker will be visible after a ping, can be set manually.
+    /// </summary>
     [SerializeField] private float durationMarkerVisible = 1.0f;
 
+    /// <summary>
+    /// Contains a reference to the input actions and is needed for the input system to function.
+    /// </summary>
     protected PingSystem _pingSystem;
+    
+    /// <summary>
+    /// Contains the position of the last ping
+    /// </summary>
     protected Vector3 _pingPosition;
 
+    /// <summary>
+    /// Fetches the reference to the brotherAI script.
+    /// </summary>
     protected virtual void Awake()
     {
         _brotherAI = _brother.GetComponent<BrotherAI>();
     }
     
+    /// <summary>
+    /// Calculates a ray from the camera towards the direction of the mouse.
+    /// </summary>
+    /// <returns>The ray in the direction of the mouse</returns>
     protected Ray GetRayFromCameraToMousePosition()
     {
         var mousePosition = new Vector2(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y);
@@ -27,6 +82,10 @@ public abstract class AbstractPingController : MonoBehaviour
         return ray;
     }
 
+    /// <summary>
+    /// Performs the actual ping and updates the position variable.
+    /// </summary>
+    /// <param name="ray">The ray that is used to calculate the position of a ping.</param>
     protected void SetPingPosition(Ray ray)
     {
         Debug.DrawRay(ray.origin, ray.direction, Color.red, 3);
@@ -35,26 +94,44 @@ public abstract class AbstractPingController : MonoBehaviour
         _pingPosition = hit.point;
     }
 
+    /// <summary>
+    /// Timer to destroy a ping marker after a set amount of settings.
+    /// </summary>
+    /// <param name="marker">Reference to the marker that appeared after a ping.</param>
+    /// <returns>Returns a WaitForSeconds</returns>
     protected IEnumerator MarkerDuration(GameObject marker)
     {
         yield return new WaitForSeconds(durationMarkerVisible);
         Destroy(marker);
     }
 
+    /// <summary>
+    /// Enables input actions.
+    /// </summary>
     protected void OnEnable()
     {
         _pingSystem.Enable();
     }
 
+    /// <summary>
+    /// Disables input actions.
+    /// </summary>
     protected void OnDisable()
     {
         _pingSystem.Disable();
     }
 
+    /// <summary>
+    /// Used to get the ping location.
+    /// </summary>
+    /// <returns>Ping location.</returns>
     protected Vector3 GetPingLocation()
     {
         return _pingPosition;
     }
 
+    /// <summary>
+    /// Method that should makes a marker appear.
+    /// </summary>
     protected abstract void ShowMarker(Vector3 position);
 }
--- Assets/Scripts/UI/Game interface/PingSystem/QuickPingController.cs
+++ Assets/Scripts/UI/Game interface/PingSystem/QuickPingController.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Author: Iris Giezen, Thijs Orsel </para>
+/// Modified by: n/a </para>
+///  Allows for sending the brother companion the location which he needs to run to, and shows a marker on the location of the ping for a few seconds.
+/// </summary>
+/// <list type="table">
+///	    <listheader>
+///         <term>On what GameObject</term>
+///         <term>Type</term>
+///         <term>Name of type</term>
+///         <term>Description</term>
+///     </listheader>
+///     <item>
+///         <term>Should be placed on the Player</term>
+///		    <term>Script</term>
+///         <term>QuickPingController</term>
+///		    <term>Allows for sending the brother companion the location which he needs to run to.</term>
+///	    </item>
+/// </list>
+
 public class QuickPingController : AbstractPingController
 {
     private bool _quickCancelled;
     private GameObject _marker;
-
+    
     protected override void Awake()
     {
         base.Awake();
@@ -16,6 +36,10 @@ public class QuickPingController : AbstractPingController
         _pingSystem.Player.MenuPing.started += StartOnMenuPing;
     }
 
+    /// <summary>
+    /// Gets called when left clicking, handles the finding and passing the location of the ping
+    /// </summary>
+    /// <param name="callbackContext">Information provided to action callbacks about what triggered an action. Needed to make the function react on actions.</param>
     private void OnQuickPing(InputAction.CallbackContext callbackContext)
     {
         if (_radialMenu.activeSelf || _quickCancelled) return;
@@ -34,11 +58,19 @@ public class QuickPingController : AbstractPingController
         StartCoroutine(MarkerDuration(_marker));
     }
 
+    /// <summary>
+    /// Gets called when a left click is canceled (done), is used for making sure you can't use QuickPing in the radial menu
+    /// </summary>
+    /// <param name="callbackContext">Information provided to action callbacks about what triggered an action. Needed to make the function react on actions.</param>
     private void CancelQuickPing(InputAction.CallbackContext callbackContext)
     {
         _quickCancelled = false;
     }
 
+    /// <summary>
+    /// Gets called when a right click is performed, is used for making sure you can't use QuickPing in the radial menu
+    /// </summary>
+    /// <param name="callbackContext">Information provided to action callbacks about what triggered an action. Needed to make the function react on actions.</param>
     private void StartOnMenuPing(InputAction.CallbackContext callbackContext)
     {
         _quickCancelled = true;
