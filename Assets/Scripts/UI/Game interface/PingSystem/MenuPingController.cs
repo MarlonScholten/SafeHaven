9b6fe855280d98b96c1ca95c7abea988dae94ce7
Iris Giezen <IJG.Giezen@student.han.nl>
2022-11-25 11:17:29 +0100

    Documentation MenuPingController

--- Assets/Scripts/UI/Game interface/PingSystem/MenuPingController.cs
+++ Assets/Scripts/UI/Game interface/PingSystem/MenuPingController.cs
@@ -3,12 +3,60 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
+/// <summary>
+/// Author: Thijs Orsel and Iris Giezen </para>
+/// Modified by: N/A </para>
+/// This script controls the radial menu for the pinging system.
+/// In order to let it work the 'Radial Menu' prefab should be added in the hierarchy.
+/// It places a marker on the pinged location.
+/// Also it opens and closes the radial menu and makes it possible to interact with it by the player.
+/// </summary>
+/// <list type="table">
+///	    <listheader>
+///         <term>On what GameObject</term>
+///         <term>Type</term>
+///         <term>Name of type</term>
+///         <term>Description</term>
+///     </listheader>
+///     <item>
+///         <term>Should be placed on the player (sister).</term>
+///		    <term>Script</term>
+///         <term>MenuPingController</term>
+///		    <term>The script makes it possible that the player is able to let the little brother (Brother AI) perform different actions with the help of the radial menu.</term>
+///	    </item>
+/// </list>
 public class MenuPingController : AbstractPingController
 {
+    /// <summary>
+    /// When the radial menu is active the player can hover over different actions.
+    /// The segment, on which the player hovers, changes color.
+    /// In here should be the Segment Outside, which contains all the segments that are available in the radial menu. 
+    /// </summary>
     [SerializeField] private GameObject _highlightedOption;
+
+    /// <summary>
+    /// When the radial menu is open time slows down in game.
+    /// </summary>
     [SerializeField] private int _slowmotionFactor = 4;
+
+    /// <summary>
+    /// Different parts of the radial menu have varying colors.
+    /// The radialMenuNormal controls the color of the text in the segments of the radial menu when nothing hovers over it.
+    /// The radialMenuOptionHovered controls the color of the text in the outside segment of the radial menu when the mouse hovers over it.
+    /// The radialMenuCancel controls the color of the text in the middle segment of the radial menu when the mouse hovers over it.
+    /// </summary>
     [SerializeField] private Color radialMenuNormal, radialMenuOptionHovered, radialMenuCancel;
+
+    /// <summary>
+    /// In the radial menu are different sections.
+    /// This are the different Text objects that are present in the radial menu.
+    /// </summary>
     [SerializeField] private Text[] options;
+
+    /// <summary>
+    /// In the middle of the radial menu is a cancel functionality.
+    /// In here is the Text object from the cancel option.
+    /// </summary>
     [SerializeField] private Text cancel;
 
     private Vector2 _inputMouse;
@@ -28,6 +76,9 @@ public class MenuPingController : AbstractPingController
     private const string NotCancelled = "Not cancelled";
     private const float SizeCircle = 45f;
 
+    /// <summary>
+    /// In order to let the input actions work Awake needs to be protected.
+    /// </summary>
     protected override void Awake()
     {
         base.Awake();
@@ -110,6 +161,10 @@ public class MenuPingController : AbstractPingController
         _chosenAction = pingType;
     }
 
+    /// <summary>
+    /// When the player presses the left mouse button an action can be selected or the radial menu can be cancelled.
+    /// </summary>
+    /// <param name="callbackContext">Information provided to action callbacks about what triggered an action. Needed to make the function react on actions.></param>
     private void OnLeftMouseButton(InputAction.CallbackContext callbackContext)
     {
         if (!_radialMenu.activeSelf) return;
@@ -123,9 +178,14 @@ public class MenuPingController : AbstractPingController
         {
             SelectAction();
         }
+
         CloseRadialMenu();
     }
 
+    /// <summary>
+    /// When the player presses the right mouse button a ping gets placed, time slows down and the radial menu gets activated.
+    /// </summary>
+    /// <param name="callbackContext">Information provided to action callbacks about what triggered an action. Needed to make the function react on actions.></param>
     private void OnMenuPing(InputAction.CallbackContext callbackContext)
     {
         if (_radialMenuIsSetActive) return;
@@ -138,6 +198,11 @@ public class MenuPingController : AbstractPingController
         Time.timeScale /= _slowmotionFactor;
     }
 
+    /// <summary>
+    /// The chosen action from the player gets set to the ping action.
+    /// The ping action together with the ping position will be passed on to the Brother AI.
+    /// This is the place where the integration with the brother AI takes place.
+    /// </summary>
     private void SelectAction()
     {
         _pingAction = _chosenAction;
@@ -153,6 +218,13 @@ public class MenuPingController : AbstractPingController
         Time.timeScale = StandardTimeFactor;
     }
 
+    /// <summary>
+    /// ShowMarker is present in the abstract class AbstractPingController.
+    /// This is why it is protected.
+    /// It overrides the method that is present in that class.
+    /// A marker gets instantiated on the pinged position.
+    /// </summary>
+    /// <param name="position">The position the player pings.</param>
     protected override void ShowMarker(Vector3 position)
     {
         _marker = Instantiate(_markerPrefab, position, Quaternion.identity);
