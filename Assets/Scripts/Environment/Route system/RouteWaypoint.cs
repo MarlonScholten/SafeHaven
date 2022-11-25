b2af17c020648895050fb5a3b4207a0f235251fa
Hugo Verweij <kajirinnn@gmail.com>
2022-11-25 14:29:02 +0100

    Updated route waypoint.

--- Assets/Scripts/Environment/Route system/RouteWaypoint.cs
+++ Assets/Scripts/Environment/Route system/RouteWaypoint.cs
@@ -1,30 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class RouteWaypoint : MonoBehaviour
 {
-    [Header("Events")]
-    public UnityEvent WaypointReached;
-    public float Velocity => _velocity;
+    public float Speed => _speed;
     public float WaitTime => _waitTime;
 
     [Header("Settings")]
     [SerializeField]
-    [Tooltip("The velocity the object will have from here on out.")]
-    private float _velocity = 10;
+    [Tooltip("The speed the object will have from here on out.")]
+    private float _speed;
     [SerializeField]
     [Tooltip("The time the object will take to wait at this waypoint.")]
     private float _waitTime;
-
-    private void Start()
-    {
-        
-    }
-
-    private void Update()
-    {
-        
-    }
 }
