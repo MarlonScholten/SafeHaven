882bd960fb4ebaffb22a2733ea2ae2bbc1c71a08
Sami Lappalainen <sami.lappalainen@hotmail.fi>
2022-11-29 19:54:43 +0100

    Sounds and Music for Terrible Thursday

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,28 +13,31 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_PROXLAMP_DRAFT1 = 1027248228U;
-        static const AkUniqueID PLAY_PROXLAMP_DRAFT1_BUZZ = 1848665912U;
-        static const AkUniqueID PLAY_PROXLAMP_DRAFT1_BUZZ2 = 2922544410U;
-        static const AkUniqueID PLAY_PROXLAMP_DRAFT1_EXITCLICK = 118004385U;
-        static const AkUniqueID PLAY_PROXLAMP_DRAFT1_HUM = 752515743U;
-        static const AkUniqueID PLAY_PROXLAMP_DRAFT1_HUM2 = 988743807U;
+        static const AkUniqueID PLAY_PROXLAMP_DRAFT1_CLICK = 2579192193U;
+        static const AkUniqueID PLAY_PROXLAMP_DRAFT1_V2 = 1416161311U;
+        static const AkUniqueID PLAY_STEALTH_MUSIC = 674798631U;
         static const AkUniqueID PLAY_TESTSOUND_LOOP = 2024687188U;
         static const AkUniqueID PLAY_TESTSOUND_SINGLE = 4206196186U;
+        static const AkUniqueID PLAY_THEME = 2479442773U;
+        static const AkUniqueID PLAY_THEME2 = 4209088253U;
+        static const AkUniqueID PLAY_THEME3 = 4209088252U;
+        static const AkUniqueID PLAY_THEME4 = 4209088251U;
         static const AkUniqueID PLAY_WIND_PLACEHOLDER = 3498798050U;
-        static const AkUniqueID STOP_PROXLAMP_DRAFT1 = 4116403402U;
-        static const AkUniqueID STOP_PROXLAMP_DRAFT1_BUZZ = 408457746U;
-        static const AkUniqueID STOP_PROXLAMP_DRAFT1_BUZZ2 = 1701704292U;
-        static const AkUniqueID STOP_PROXLAMP_DRAFT1_HUM = 4294825585U;
-        static const AkUniqueID STOP_PROXLAMP_DRAFT1_HUM2 = 1838715857U;
+        static const AkUniqueID STOP_PROXLAMP_DRAFT1_V2 = 2594172909U;
+        static const AkUniqueID STOP_STEALTH_MUSIC = 3876103401U;
         static const AkUniqueID STOP_TESTSOUND_LOOP = 3467245854U;
         static const AkUniqueID STOP_TESTSOUND_SINGLE = 25015968U;
+        static const AkUniqueID STOP_THEME = 560299279U;
+        static const AkUniqueID STOP_THEME2 = 2713968303U;
+        static const AkUniqueID STOP_THEME3 = 2713968302U;
+        static const AkUniqueID STOP_THEME4 = 2713968297U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MENU = 2607556080U;
     } // namespace BANKS
 
     namespace BUSSES
