194f5f6ebbc5d51d53bacad37f09f2a145caf636
HugUlf <hugo.ulfman@gmail.com>
2022-12-01 10:58:43 +0100

    Added step by step guide to proximity

--- Assets/Scripts/Environment/Proximity triggers/Interfaces/ITrigger/LightTrigger.cs
+++ Assets/Scripts/Environment/Proximity triggers/Interfaces/ITrigger/LightTrigger.cs
@@ -7,7 +7,14 @@ using UnityEngine;
 /// Modified by:  </br>
 /// This class implements the ITrigger interface and is used to activate a light in the scene.
 /// When the trigger() method is called, the light will be activated.
-/// An object with this script can be added to the list of the triggerboxes.
+/// How to add LightTrigger to scene:
+/// 1. Add a light to the scene and turn it off.
+/// 2. Add this script to the light.
+/// 3. Create a trigger box of some kind to the scene.
+/// 4. Add the TriggerObject script to the trigger box.
+/// 5. Set the trigger box to IsTrigger.
+/// 6. Add the light to the TriggerObject's TriggeredObjects list.
+/// 7. When the player enters the trigger box, the light will be activated.
 /// </summary>
 public class LightTrigger : MonoBehaviour, ITrigger
 {
