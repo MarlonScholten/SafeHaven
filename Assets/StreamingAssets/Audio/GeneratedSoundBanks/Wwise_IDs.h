11c2468770a5e235949e40298207de8f68556d05
Sami Lappalainen <sami.lappalainen@hotmail.fi>
2022-12-07 12:41:42 +0100

    Added Footsteps, deleted some placeholder files
    Deleted Stop and Play events for:
    Theme
    Theme3
    Theme4
    Proxlamp_Draft1
    
    Added:
    Play_Footsteps - with a switch group Footsteps for Grass, Gravel and Concrete switches

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,25 +13,36 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
         static const AkUniqueID PLAY_PROXLAMP_DRAFT1_CLICK = 2579192193U;
         static const AkUniqueID PLAY_PROXLAMP_DRAFT1_V2 = 1416161311U;
         static const AkUniqueID PLAY_STEALTH_MUSIC = 674798631U;
         static const AkUniqueID PLAY_TESTSOUND_LOOP = 2024687188U;
         static const AkUniqueID PLAY_TESTSOUND_SINGLE = 4206196186U;
-        static const AkUniqueID PLAY_THEME = 2479442773U;
         static const AkUniqueID PLAY_THEME2 = 4209088253U;
-        static const AkUniqueID PLAY_THEME3 = 4209088252U;
-        static const AkUniqueID PLAY_THEME4 = 4209088251U;
         static const AkUniqueID PLAY_WIND_PLACEHOLDER = 3498798050U;
         static const AkUniqueID STOP_PROXLAMP_DRAFT1_V2 = 2594172909U;
         static const AkUniqueID STOP_STEALTH_MUSIC = 3876103401U;
         static const AkUniqueID STOP_TESTSOUND_LOOP = 3467245854U;
         static const AkUniqueID STOP_TESTSOUND_SINGLE = 25015968U;
-        static const AkUniqueID STOP_THEME = 560299279U;
         static const AkUniqueID STOP_THEME2 = 2713968303U;
-        static const AkUniqueID STOP_THEME3 = 2713968302U;
-        static const AkUniqueID STOP_THEME4 = 2713968297U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
