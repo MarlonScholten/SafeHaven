57047e82b64d9bc9e00ed986a7048d9de6fbc832
Sami Lappalainen <sami.lappalainen@hotmail.fi>
2022-12-07 14:54:13 +0100

    Test sounds for Parameters and States
    Added:
    Play_ParameterTest and Stop_ParameterTest - with a parameter TestParameter that goes from 0 to 100 and changes the pitch of the sound file
    
    Play_StateTest and Stop_StateTest - with a state group StateTest, which as On and Off states. These turn the sound file on and off.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,19 +14,39 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_PARAMETERTEST = 4229740275U;
         static const AkUniqueID PLAY_PROXLAMP_DRAFT1_CLICK = 2579192193U;
         static const AkUniqueID PLAY_PROXLAMP_DRAFT1_V2 = 1416161311U;
+        static const AkUniqueID PLAY_STATETEST = 3761884421U;
         static const AkUniqueID PLAY_STEALTH_MUSIC = 674798631U;
         static const AkUniqueID PLAY_TESTSOUND_LOOP = 2024687188U;
         static const AkUniqueID PLAY_TESTSOUND_SINGLE = 4206196186U;
         static const AkUniqueID PLAY_THEME2 = 4209088253U;
         static const AkUniqueID PLAY_WIND_PLACEHOLDER = 3498798050U;
+        static const AkUniqueID STOP_PARAMETERTEST = 2756796529U;
         static const AkUniqueID STOP_PROXLAMP_DRAFT1_V2 = 2594172909U;
+        static const AkUniqueID STOP_STATETEST = 2610164223U;
         static const AkUniqueID STOP_STEALTH_MUSIC = 3876103401U;
         static const AkUniqueID STOP_TESTSOUND_LOOP = 3467245854U;
         static const AkUniqueID STOP_TESTSOUND_SINGLE = 25015968U;
         static const AkUniqueID STOP_THEME2 = 2713968303U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace STATETEST
+        {
+            static const AkUniqueID GROUP = 3275448284U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+            } // namespace STATE
+        } // namespace STATETEST
+
+    } // namespace STATES
 
     namespace SWITCHES
     {
@@ -43,6 +63,11 @@ namespace AK
         } // namespace FOOTSTEPS
 
     } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TESTPARAMETER = 2010788230U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
