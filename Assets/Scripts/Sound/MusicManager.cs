26602b76156a52c72e95644caeff1549ff7f133c
Thomas <TCJ.vandenOever@student.han.nl>
2022-12-08 16:55:22 +0100

    changed some stuff for the music

--- Assets/Scripts/Sound/MusicManager.cs
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -6,9 +6,16 @@ namespace SoundManager
 {
     public class MusicManager : SoundBase
     {
+        public int test = 1;
         private void Start()
         {
             _gameObject = gameObject;
+            StartCoroutine(cororo());
+        }
+
+        IEnumerator cororo()
+        {
+            yield return new WaitForSeconds(1);
             playSound();
         }
 
--- Assets/Scripts/Sound/SoundBase/SoundBase.cs
+++ Assets/Scripts/Sound/SoundBase/SoundBase.cs
@@ -42,68 +42,25 @@ namespace SoundManager
 
         protected GameObject _gameObject;
 
-        private AK.Wwise.RTPC volumeRTPC;
-
-        private AK.Wwise.RTPC attenuationRTPC;
-
-        private bool _isPlaying = false;
-
         protected virtual void playSound(GameObject soundGameObject = null)
         {
-            Debug.Log("Start Playing Sound");
             if(soundGameObject != null)
             {
                 _gameObject = soundGameObject;
             }
 
-            if (_isPlaying)
-            {
-                return;
-            }
-            playEvent.Post(_gameObject, (uint)AkCallbackType.AK_EndOfEvent, soundCallback);
-            _isPlaying = true;
+            playEvent.Post(_gameObject);
         }
 
         protected virtual void stopSound(int transitionTime = 0)
         {
             if (stopEvent.IsValid())
             {
-                //Debug.Log("post the stop event");
                 stopEvent.Post(_gameObject);
             }
             else
             {
-                //Debug.Log("stop current event");
                 playEvent.Stop(_gameObject, transitionTime);
-            }
-        }
-
-        protected virtual int changeVolume(int volume)
-        {
-            if (volume > 0 || volume < 100)
-            {
-                _volume = volume;
-            }
-            volumeRTPC.SetGlobalValue(_volume);
-            return _volume;
-        }
-
-        protected virtual int changeAttenuation(int attenuation)
-        {
-            if (attenuation > 0 || attenuation < 100)
-            {
-                _attenuation = attenuation;
-            }
-            attenuationRTPC.SetGlobalValue(_attenuation);
-            return _attenuation;
-        }
-
-        protected virtual void soundCallback(object in_cookie, AkCallbackType in_type, object in_info)
-        {
-            //Debug.Log(in_type);
-            if(in_type == AkCallbackType.AK_EndOfEvent)
-            {
-                _isPlaying = false;
             }
         }
     }
