0083e87129ca2dfbd9cf9302ab0ff789ea2e891d
Thomas <TCJ.vandenOever@student.han.nl>
2022-12-19 12:56:28 +0100

    added docu to OnEventTrigger.cs

--- Assets/Wwise/MonoBehaviour/Runtime/OnEventTrigger.cs
+++ Assets/Wwise/MonoBehaviour/Runtime/OnEventTrigger.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Author: Thomas van den OEver <br/>
+/// Modified by:  <br/>
+/// Description: A trigger function to call for sound events
+/// </summary>
 public class OnEventTrigger : AkTriggerBase
 {
     private void Start()
