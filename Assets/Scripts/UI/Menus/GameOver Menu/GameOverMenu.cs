a643d4690528e16d9a0f5a76d3327dcccb2a40b7
Kajirinn <kajirinnn@gmail.com>
2022-12-20 15:08:33 +0100

    Basic menu settings.

---
+++ Assets/Scripts/UI/Menus/GameOver Menu/GameOverMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverMenu : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    [Tooltip("The retry, 'start again' button. Should restart the level.")]
+    private Button _retry;
+
+    [SerializeField]
+    [Tooltip("The return, 'return to menu' button. Should return you to the menu.")]
+    private Button _return;
+
+    [Header("Duplicate")]
+    [SerializeField]
+    [Tooltip("The prefab that contains the UI for the loading screen.")]
+    private GameObject _loadingScreen;
+
+    [SerializeField]
+    [Tooltip("The id from the scene to load when the main menu button is pressed. The scene needs to be added to the build settings, the id is the place of the scene in that list.")]
+    private int _mainMenuSceneId;
+
+    private void Start()
+    {
+        _retry.onClick.AddListener(OnRetry);
+        _return.onClick.AddListener(OnReturn);
+
+        _retry.Select();
+    }
+
+    private void OnRetry()
+    {
+
+    }
+
+    private void OnReturn()
+    {
+        Instantiate(_loadingScreen);
+        GameObject.Find("EnemyStateWatcher").GetComponent<SoundManager.EnemyStateWatcher>().StopSound();
+        SceneManager.LoadSceneAsync(_mainMenuSceneId, LoadSceneMode.Single);
+    }
+}
